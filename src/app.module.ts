23e2d558ce49b9e3f097e6d5a7e7223e2f6012b5
matheus <matheuswanderley1988@gmail.com>
2022-11-23 16:52:56 -0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://root:root@cluster0.vjbme.mongodb.net/?retryWrites=true&w=majority'), UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
