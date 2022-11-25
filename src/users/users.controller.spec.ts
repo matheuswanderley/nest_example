6956c05ead4dd28a48304d5ad5ee0cfedc24edc1
matheus <matheuswanderley1988@gmail.com>
2022-11-25 14:24:53 -0300

    unit test, controller 100%

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,16 +5,107 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    create: jest.fn((dto) => {
+      return { _id: Date.now().toString(), ...dto };
+    }),
+    findAll: jest.fn((dto) => {
+      return {
+        _id: '11111111',
+        name: 'matheus',
+        email: 'matheus@matheus',
+        password: '12345',
+      };
+    }),
+    findOne: jest.fn((dto) => {
+      return {
+        _id: '1',
+        name: 'matheus',
+        email: 'matheus@matheus',
+        password: '12345',
+      };
+    }),
+    update: jest.fn((_id, dto) => {
+      return {
+        _id: '1',
+        name: 'matheus',
+        email: 'matheus@matheus',
+        password: '12345',
+      };
+    }),
+    remove: jest.fn((_id, dto) => {
+      return {
+        _id: '1',
+        name: 'matheus',
+        email: 'matheus@matheus',
+        password: '12345',
+      };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should be create a user', () => {
+    const dto = {
+      _id: expect.any(String),
+      name: 'matheus',
+      email: 'matheus@matheus',
+      password: '12345',
+    };
+    expect(controller.create(dto)).toEqual(dto);
+    expect(mockUsersService.create).toHaveBeenCalled();
+  });
+
+  it('should be findall a users', () => {
+    const dto = {
+      _id: expect.any(String),
+      name: 'matheus',
+      email: 'matheus@matheus',
+      password: '12345',
+    };
+    expect(controller.findAll()).toEqual(dto);
+    expect(mockUsersService.findAll).toHaveBeenCalled();
+  });
+
+  it('should be find one users', () => {
+    const dto = {
+      _id: expect.any(String),
+      name: 'matheus',
+      email: 'matheus@matheus',
+      password: '12345',
+    };
+    expect(controller.findOne('1')).toEqual(dto);
+    expect(mockUsersService.findOne).toHaveBeenCalled();
+  });
+
+  it('should be update one users', () => {
+    const dto = {
+      _id: expect.any(String),
+      name: 'matheus',
+      email: 'matheus@matheus',
+      password: '12345',
+    };
+    expect(controller.update('1', dto)).toEqual(dto);
+    expect(mockUsersService.update).toHaveBeenCalled();
+  });
+
+  it('should be delete one users', () => {
+    const dto = {
+      _id: expect.any(String),
+      name: 'matheus',
+      email: 'matheus@matheus',
+      password: '12345',
+    };
+    expect(controller.remove('1')).toEqual(dto);
+    expect(mockUsersService.remove).toHaveBeenCalled();
   });
 });
