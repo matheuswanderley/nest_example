d1a81ea542b7ed8e23ea297410338847d208db90
matheus <matheuswanderley1988@gmail.com>
2022-11-28 10:55:12 -0300

    refact test users.controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,111 +1,92 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { async } from 'rxjs';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
-
-  const mockUsersService = {
-    create: jest.fn((dto) => {
-      return { _id: Date.now().toString(), ...dto };
-    }),
-    findAll: jest.fn((dto) => {
-      return {
-        _id: '11111111',
-        name: 'matheus',
-        email: 'matheus@matheus',
-        password: '12345',
-      };
-    }),
-    findOne: jest.fn((dto) => {
-      return {
-        _id: '1',
-        name: 'matheus',
-        email: 'matheus@matheus',
-        password: '12345',
-      };
-    }),
-    update: jest.fn((_id, dto) => {
-      return {
-        _id: '1',
-        name: 'matheus',
-        email: 'matheus@matheus',
-        password: '12345',
-      };
-    }),
-    remove: jest.fn((_id, dto) => {
-      return {
-        _id: '1',
-        name: 'matheus',
-        email: 'matheus@matheus',
-        password: '12345',
-      };
-    }),
+  const dto = {
+    _id: '1',
+    name: 'matheus',
+    email: 'matheus@matheus',
+    password: '12345',
   };
+  class UserModel {
+    constructor(private dto) {}
+    create = jest.fn().mockResolvedValue(this.dto);
+    static findAll = jest.fn().mockResolvedValue(dto);
+    static findOne = jest.fn().mockResolvedValue(dto);
+    static update = jest.fn().mockResolvedValue(dto);
+    static remove = jest.fn().mockResolvedValueOnce(dto);
+    save = jest.fn().mockResolvedValue(this.dto);
+    static find = jest.fn().mockResolvedValue(dto);
+    static findById = jest.fn().mockResolvedValue(dto);
+    static findByIdAndUpdate = jest.fn().mockResolvedValue(dto);
+    static deleteOne = jest.fn().mockResolvedValueOnce(dto);
+    static exec = jest.fn();
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [UsersService, {
+        provide: getModelToken('User'),
+        useValue: UserModel
+      }],
     })
-      .overrideProvider(UsersService)
-      .useValue(mockUsersService)
       .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be create a user', () => {
+  it('should be create a user', async() => {
     const dto = {
       _id: expect.any(String),
       name: 'matheus',
       email: 'matheus@matheus',
       password: '12345',
     };
-    expect(controller.create(dto)).toEqual(dto);
-    expect(mockUsersService.create).toHaveBeenCalled();
+    expect(await controller.create(dto)).toEqual(dto);
   });
 
-  it('should be findall a users', () => {
+  it('should be findall a users', async() => {
     const dto = {
       _id: expect.any(String),
       name: 'matheus',
       email: 'matheus@matheus',
       password: '12345',
     };
-    expect(controller.findAll()).toEqual(dto);
-    expect(mockUsersService.findAll).toHaveBeenCalled();
+    expect(await controller.findAll()).toEqual(dto);
   });
 
-  it('should be find one user', () => {
+  it('should be find one user', async() => {
     const dto = {
       _id: expect.any(String),
       name: 'matheus',
       email: 'matheus@matheus',
       password: '12345',
     };
-    expect(controller.findOne('1')).toEqual(dto);
-    expect(mockUsersService.findOne).toHaveBeenCalled();
+    expect(await controller.findOne('1')).toEqual(dto);
   });
 
-  it('should be update one user', () => {
+  it('should be update one user', async() => {
     const dto = {
       _id: expect.any(String),
       name: 'matheus',
       email: 'matheus@matheus',
       password: '12345',
     };
-    expect(controller.update('1', dto)).toEqual(dto);
-    expect(mockUsersService.update).toHaveBeenCalled();
+    expect(await controller.update('1', dto)).toEqual(dto);
   });
 
-  it('should be delete one user', () => {
+  it('should be delete one user', async() => {
     const dto = {
       _id: expect.any(String),
       name: 'matheus',
       email: 'matheus@matheus',
       password: '12345',
     };
-    expect(controller.remove('1')).toEqual(dto);
-    expect(mockUsersService.remove).toHaveBeenCalled();
+    expect(await controller.remove('1')).toEqual(dto);
   });
 });
